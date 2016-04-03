2c955b22d7775402e2caf1b3b1c33fca2290d860
bortevik <bortevik@gmail.com>
2016-04-03 10:50:02 +0500

    Serialize updated_at for questions and answers

--- web/serializers/api/v1/answer_serializer.ex
+++ web/serializers/api/v1/answer_serializer.ex
@@ -2,7 +2,7 @@ defmodule Howtosay.Api.V1.AnswerSerializer do
   use JaSerializer
 
   location "/api/v1/answers/:id"
-  attributes [:text, :inserted_at]
+  attributes [:text, :inserted_at, :updated_at]
 
   has_one :question, field: :question_id, type: "question"
   has_one :user, field: :user_id, type: "user"
--- web/serializers/api/v1/question_serializer.ex
+++ web/serializers/api/v1/question_serializer.ex
@@ -2,7 +2,7 @@ defmodule Howtosay.Api.V1.QuestionSerializer do
   use JaSerializer
 
   location "/api/v1/questions/:id"
-  attributes [:text, :inserted_at]
+  attributes [:text, :inserted_at, :updated_at]
 
   has_one :language_from, field: :language_from_id, type: "language"
   has_one :language_to, field: :language_to_id, type: "language"
