b9705d9fa0c03632990b606e9026c88a9b143011
bortevik <bortevik@gmail.com>
2016-08-11 13:11:37 +0500

    Serialize answers count with question

--- web/serializers/api/v1/question_serializer.ex
+++ web/serializers/api/v1/question_serializer.ex
@@ -1,8 +1,12 @@
 defmodule Howtosay.Api.V1.QuestionSerializer do
   use JaSerializer
 
+  alias Howtosay.Repo
+  import Ecto.Query, only: [where: 2, select: 3]
+  import Ecto, only: [assoc: 2]
+
   location "/api/v1/questions/:id"
-  attributes [:text, :votes, :inserted_at, :updated_at]
+  attributes [:text, :votes, :inserted_at, :updated_at, :answers_count]
 
   has_one :language_from, field: :language_from_id, type: "language"
   has_one :language_to, field: :language_to_id, type: "language"
@@ -14,10 +18,17 @@ defmodule Howtosay.Api.V1.QuestionSerializer do
     case question.user do
       %Ecto.Association.NotLoaded{} ->
         question
-        |> Ecto.assoc(:user)
-        |> Howtosay.Repo.one()
+        |> assoc(:user)
+        |> Repo.one()
       other -> other
     end
+  end
+
+  def answers_count(question, _conn) do
+    Howtosay.Answer
+    |> where(question_id: ^question.id)
+    |> select([a], count(a.id))
+    |> Repo.one()
   end
 end
 
